fix: normalization for numpy.bool_

cpmpy/transformations/normalize.py
@@ -24,10 +24,11 @@ def toplevel_list(cpm_expr, merge_and=True):
                     append(e) # presumably the most frequent case
             elif isinstance(e, (list, tuple, np.flatiter, np.ndarray)):
                 unravel(e, append)
-            elif e is False:
-                append(BoolVal(e))
-            elif e is not True:  # if True: pass
-                raise NotSupportedError(f"Expression {e} is not a valid CPMpy constraint")
+            elif e is False or e is np.False_:
+                append(BoolVal(False))
+            elif e is not True and e is not np.True_:  # if True: pass
+                raise NotSupportedError(
+                    f"Expression {e} is not a valid CPMpy constraint")
 
     newlist = []
     append = newlist.append